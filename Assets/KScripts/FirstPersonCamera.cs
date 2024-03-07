dad63cc43cb5979c3c4d9609531ff5ac43a4ae9d
KimbCruz <kcruz92@toromail.csudh.edu>
2024-03-07 15:30:37 -0800

    Camera Development
    I also added Janitor and an office spawn which is a basic flooring for the janitor

---
+++ Assets/KScripts/FirstPersonCamera.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    //Variables
+    public Transform janitor;
+    public float mouseX;
+    public float mouseY;
+    float cameraYRotation;
+    float cameraXRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mouse direction
+        float x = Input.GetAxis("Mouse X") * Time.deltaTime * mouseX;
+        float y = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseY;
+
+        //Rotation of the Camera of its x axis
+        cameraYRotation += x;
+        //Rotation of the Player Object and the Camera of it y axis
+        cameraXRotation -= y;
+        cameraXRotation = Mathf.Clamp(cameraXRotation, -90f, 90f);
+
+        //rotate
+        transform.rotation = Quaternion.Euler(cameraXRotation, cameraYRotation, 0);
+        janitor.rotation = Quaternion.Euler(0, cameraYRotation, 0);
+
+    }
+}
---
+++ Assets/KScripts/MoveCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    //Variables
+    //Camera position
+    public Transform cameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Update the camera position with the current camera position
+        transform.position = cameraPos.position;
+    }
+}
