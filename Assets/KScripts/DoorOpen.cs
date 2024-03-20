08d00731bfbe546a9961da0fd2181e888a73354a
KimbCruz <kcruz92@toromail.csudh.edu>
2024-03-19 19:32:46 -0700

    Made Door Animations

---
+++ Assets/KScripts/DoorOpen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    //Animation of the Doors
+    public Animator animator;
+    public Transform janitor;
+    public Transform doors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
