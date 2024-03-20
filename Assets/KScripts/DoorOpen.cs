312c9dd9389c10d1cbb6e984ef811ced0aea2d0a
KimbCruz <kcruz92@toromail.csudh.edu>
2024-03-20 14:51:21 -0700

    Door Animation
    The doors animate when the janitor is near and when all trash is collected.

--- Assets/KScripts/DoorOpen.cs
+++ Assets/KScripts/DoorOpen.cs
@@ -18,6 +18,16 @@ public class DoorOpen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Calculate the distance from the player to the SciFi doors
+        float nearDistance = Vector3.Distance(janitor.position, doors.position);
+        //if the distance is near the doors start animation and open doors
+        if(nearDistance <= 20 && RubbishManager.rubbishCount >= 5)
+        {
+            animator.SetBool("Near", true);
+        }
+        else
+        {
+            animator.SetBool("Near", false);
+        }
     }
 }
